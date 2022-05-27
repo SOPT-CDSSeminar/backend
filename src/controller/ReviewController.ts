796e5038653249608d3fa1399a71ad70b7af4445
DongLee99 <55686088+DongLee99@users.noreply.github.com>
2022-05-27 17:36:14 +0900

    feat: review create 구현

--- src/controller/ReviewController.ts
+++ src/controller/ReviewController.ts
@@ -21,6 +21,25 @@ const getSortedReviews = async (req: Request, res: Response) => {
     }
 }
 
+const createReview = async (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty) {
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.BAD_REQUEST));
+    }
+    console.log("요청 들어옴");
+    const reviewCreateDto: ReviewCreateDto = req.body;
+    try {
+        const data = await ReviewService.createReview(reviewCreateDto);
+        if(!data) res.status(statusCode.NOT_FOUND).send(util.fail(statusCode.NOT_FOUND, responseMessage.NOT_FOUND));
+        res.status(statusCode.CREATED).send(util.success(statusCode.CREATED, responseMessage.CREATE_REVIEW_SUCCESS, data));
+    
+    } catch (error) {
+        console.log(error)
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    }
+}
+
 export default {
-    getSortedReviews
+    getSortedReviews,
+    createReview
 }
